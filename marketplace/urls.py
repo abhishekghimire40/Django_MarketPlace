b231b2511ea4b2c5f2bd7b82600ddb987b1eefa0
Abhishek Ghimire <abhishekghimire@pop-os.localdomain>
2023-07-15 11:11:44 +0545

    created simple frontPage/homePage that displays items and categories

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -1,3 +1,7 @@
+# for handling images
+from django.conf import settings
+from django.conf.urls.static import static
+# NOTE: don't handle images like this in production environment
 
 from django.contrib import admin
 from django.urls import path
@@ -7,4 +11,4 @@ urlpatterns = [
     path('',index,name="index"),
     path('contact/',contact,name="contact"),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
