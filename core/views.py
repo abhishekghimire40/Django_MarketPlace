830c60e969f87ba2a9b89332b5c439a3e078c7ae
Abhishek Ghimire <abhishekghimire@pop-os.localdomain>
2023-07-16 12:14:29 +0545

    created signup form template and also applied validation to create a user in our db

--- core/views.py
+++ core/views.py
@@ -1,13 +1,31 @@
-from django.shortcuts import render
-from item.models import Category,Item
+from django.shortcuts import render, redirect
+from item.models import Category, Item
+from .forms import SignupForm
+
 
 # Create your views here.
+# function/view to handle homepage
 def index(request):
-  items = Item.objects.filter(is_sold=False)[0:6]
-  categories = Category.objects.all()
-  return render(request,'core/index.html',{
-     'categories':categories,"items":items
-  })
+    items = Item.objects.filter(is_sold=False)[0:6]
+    categories = Category.objects.all()
+    return render(
+        request, "core/index.html", {"categories": categories, "items": items}
+    )
 
+
+# function/view to handle contact
 def contact(request):
-    return render(request,"core/contact.html")
+    return render(request, "core/contact.html")
+
+
+# view to handle form
+def signup(request):
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/login/")
+
+    else:
+        form = SignupForm()
+    return render(request, "core/signup.html", {"form": form})
