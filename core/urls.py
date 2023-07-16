610b9d20ff2685a952c53114f2b6927f64620405
Abhishek Ghimire <abhishekghimire@pop-os.localdomain>
2023-07-16 12:32:14 +0545

    login form created and handled using django built-in login view

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,9 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
+
 from . import views
+from .forms import LoginForm
+
 
 app_name = "core"
 
@@ -7,4 +11,14 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("contact/", views.contact, name="contact"),
     path("signup/", views.signup, name="signup"),
+    # used django built in login view to handle login
+    # NOTE: when logged in, it redirect to /accounts/profile so make sure to redirect it
+    # to a path you want in settings.py file
+    path(
+        "login/",
+        auth_views.LoginView.as_view(
+            template_name="core/login.html", authentication_form=LoginForm
+        ),
+        name="login",
+    ),
 ]
